be448a5d951cfefc4869ed81686ff1fb517634dd
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-02-19 13:54:01 +0100

    new

--- src/testing.js
+++ src/testing.js
@@ -1,11 +1,11 @@
 // import exam from './data.js'
 
-export const newArr = {
-  id: 22,
-  title: 'Ayoub Maimmadi',
-  category: 'AA ',
-  name: 'GG 3300',
-  img: './images/ims.jpg',
-  link: 'https://github.com/AyoubMaimmadi',
-  desc: 'Lorem ipsum dolor sit amet.',
-}
+// export const newArr = {
+//   id: 22,
+//   title: 'Ayoub Maimmadi',
+//   category: 'AA ',
+//   name: 'GG 3300',
+//   img: './images/ims.jpg',
+//   link: 'https://github.com/AyoubMaimmadi',
+//   desc: 'Lorem ipsum dolor sit amet.',
+// }
