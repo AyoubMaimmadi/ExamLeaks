0fd618319b4562133a2a0b6f572868aec3b709c1
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-02-20 22:16:22 +0100

    new

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,8 +1,53 @@
 import React, { useState, useEffect } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoteRight } from 'react-icons/fa'
+import data from './data/ReviewsData'
 import './css/reviews.css'
 
 const Reviews = () => {
-  return <div className="text-center">Reviews!!</div>
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = React.useState(0)
+
+  return (
+    <section className="section-review">
+      <div className="title-review">
+        <h2 className="text-center">
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center-review">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person
+
+          let position = 'nextSlide'
+          if (personIndex === index) {
+            position = 'activeSlide'
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide'
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title-review">{title}</p>
+              <p className="text-review">{quote}</p>
+            </article>
+          )
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  )
 }
 
 export default Reviews
---
+++ src/data/ReviewsData.js
@@ -0,0 +1,36 @@
+const reviews = [
+  {
+    id: 1,
+    image: './images/ayoub.jpeg',
+    name: 'Ayoub Maimmadi',
+    title: 'SSE Student',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 2,
+    image: './images/ayoub.jpeg',
+    name: 'Rabii Alaoui',
+    title: 'SSE Student',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+  {
+    id: 3,
+    image: './images/ayoub.jpeg',
+    name: 'Imane Ettabae',
+    title: 'SSE Student',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 4,
+    image: './images/ayoub.jpeg',
+    name: 'Hiba Akhadar',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag.',
+  },
+]
+
+export default reviews
