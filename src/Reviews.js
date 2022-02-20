e0601b4dd4109d772695f218097ef2cc79827549
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-02-20 18:44:43 +0100

    new

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,16 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import './css/reviews.css'
 
-import { MdPreview } from 'react-icons/fa'
-
 const Reviews = () => {
-  return (
-    <div className="text-center">
-      Reviews!!
-      <br />
-      <MdPreview />
-    </div>
-  )
+  return <div className="text-center">Reviews!!</div>
 }
 
 export default Reviews
