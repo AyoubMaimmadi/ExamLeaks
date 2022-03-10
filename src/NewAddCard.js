36a8319cd54a0c548088694bb240b711d05d6675
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-03-10 13:17:22 +0100

    new

--- src/NewAddCard.js
+++ src/NewAddCard.js
@@ -28,6 +28,10 @@ const NewAddCard = () => {
     `You must have an AUI ID to leave a review!`
   )
   const [colorError3, setColorError3] = useState(false)
+  const [username, setusername] = useState('')
+  const [school, setschool] = useState('')
+  const [review, setreview] = useState('')
+  const [auiID, setauiID] = useState('')
 
   let id = new Date().getTime().toString()
 
@@ -37,7 +41,6 @@ const NewAddCard = () => {
   }
 
   // Store data we got from the form in an item and add it to the list of Exams
-
   const handleSendData = () => {
     const myitem = { id, title, category, name, img, link, desc }
     if (key !== password) {
@@ -85,6 +88,32 @@ const NewAddCard = () => {
     }
   }
 
+  const handleReview = (e) => {
+    e.preventDefault()
+    // if (parseInt(auiID) == 0) {
+    //   setTimeout(() => setColorError3(false), 800)
+    //   setColorError3(true)
+    //   setauiID('')
+    //   setTimeout(() => setReviewError(`Only AUIers can leave reviews :/`), 800)
+    //   setReviewError(`ID doesn't exist`)
+    //   setauiID('')
+    // }
+    if (parseInt(auiID) >= 7000) {
+      setusername('')
+      setschool('')
+      setreview('')
+      setauiID('')
+      setReviewError('Review Added!')
+    } else {
+      setTimeout(() => setColorError3(false), 800)
+      setColorError3(true)
+      setauiID('')
+      setTimeout(() => setReviewError(`Only AUIers can leave reviews :/`), 800)
+      setReviewError(`ID doesn't exist`)
+      setauiID('')
+    }
+  }
+
   // useEffect(() => {
   //   const existingData = localStorage.getItem('item')
   //   setExist(existingData ? JSON.parse(existingData) : [])
@@ -103,7 +132,7 @@ const NewAddCard = () => {
         <br />
         <div className="title">
           <h2 id="Add">
-            Add/Del Card
+            Add Card
             <a className="scroll-link"></a>
           </h2>
           <div className="underline"></div>
@@ -306,11 +335,11 @@ const NewAddCard = () => {
       <br />
       <div className="uperuper">
         <div className="container myowncontainer">
-          <form onSubmit={EditData}>
+          <form onSubmit={handleReview}>
             <div className="row pt-5 mx-auto">
               <div className="col-8 form-group pt-2 mx-auto">
                 <div className="title">
-                  <h2 id="Add">Leave a Review</h2>
+                  <h2 id="Add">Leave Review</h2>
                   <div className="underline"></div>
                   <br />
                 </div>
@@ -322,10 +351,10 @@ const NewAddCard = () => {
                   type="text"
                   required
                   className="form-control"
-                  placeholder="Item ID"
-                  value={ItemID}
+                  placeholder="Full Name"
+                  value={username}
                   onChange={(e) => {
-                    setItemID(e.target.value)
+                    setusername(e.target.value)
                   }}
                 />
               </div>
@@ -333,13 +362,39 @@ const NewAddCard = () => {
                 <input
                   type="text"
                   required
+                  className="form-control"
+                  placeholder="AUI School"
+                  value={school}
+                  onChange={(e) => {
+                    setschool(e.target.value)
+                  }}
+                />
+              </div>
+              <div className="col-8 form-group pt-2 mx-auto">
+                <input
+                  type="text"
+                  required
+                  className="form-control"
+                  placeholder="Review"
+                  value={review}
+                  onChange={(e) => {
+                    setreview(e.target.value)
+                  }}
+                />
+              </div>
+              <div className="col-8 form-group pt-2 mx-auto">
+                <input
+                  type="number"
+                  required
                   className={`${
                     colorError3 ? 'form-control btn-danger' : 'form-control'
                   }`}
-                  placeholder="Admin key"
-                  value={Delkey}
+                  placeholder="AUI ID"
+                  // min="7000"
+                  max="100500"
+                  value={auiID}
                   onChange={(e) => {
-                    setDelKey(e.target.value)
+                    setauiID(e.target.value)
                   }}
                 />
               </div>
