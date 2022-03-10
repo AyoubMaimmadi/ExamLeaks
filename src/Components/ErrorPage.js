71d0ff594a0d3cd13a6bc0265d1fedc0e5db0b3a
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-03-10 12:20:44 +0100

    new

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import backHome from '../css/backHome.css'
 import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Error</h1>
-      <Link to="/">Back Home</Link>
+    <div class="mainbox">
+      <div class="err">4</div>
+      <div class="far">0</div>
+      <div class="err2">4</div>
+      <div class="msg">
+        Maybe this page moved? Got deleted? Is hiding out somewhere? Never
+        existed in the first place?
+        <br />
+        <br />
+        <p>
+          Let's go <Link to="/">home</Link> and try from there.
+        </p>
+      </div>
     </div>
   )
 }
