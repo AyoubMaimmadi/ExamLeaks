8f770ac4ec855793106dbb2c7782e222bc6082a7
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-03-13 16:08:13 +0100

    new

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import backHome from '../css/backHome.css'
+import errorpage from '../css/errorpage.css'
 import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
