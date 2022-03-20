69cf45684601581a41602cb60593b72070659ad4
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-03-20 14:58:32 +0100

    new

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -16,6 +16,16 @@ export const createPost = (post) => async (dispatch) => {
 
     dispatch({ type: 'CREATE', payload: data })
   } catch (error) {
-    console.log(error.message)
+    console.log(error)
+  }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id)
+
+    dispatch({ type: 'DELETE', payload: id })
+  } catch (error) {
+    console.log(error)
   }
 }
