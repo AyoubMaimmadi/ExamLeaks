38d0665f83bc94ed1c79cc4d8b00459d52e72ab2
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-03-28 15:16:06 +0000

    new

--- client/src/Components/Email.js
+++ client/src/Components/Email.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Navbar from './Navbar'
 import SearchBar from './SearchBar'
-import { Link } from 'react-router-dom'
 import emailjs from 'emailjs-com'
 
 const Email = () => {
