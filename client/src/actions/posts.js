6d669fb3de37d058339c7bb2d64fbed1f3beaef6
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-12 11:13:16 +0000

    new

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -6,7 +6,7 @@ export const getPosts = () => async (dispatch) => {
     const { data } = await api.fetchPosts()
     dispatch({ type: 'FETCH_ALL', payload: data })
   } catch (error) {
-    console.log(error.message)
+    console.error(error)
   }
 }
 
@@ -15,7 +15,7 @@ export const createPost = (post) => async (dispatch) => {
     const { data } = await api.createPost(post)
     dispatch({ type: 'CREATE', payload: data })
   } catch (error) {
-    console.log(error)
+    console.error(error)
   }
 }
 
