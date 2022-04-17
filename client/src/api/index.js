80e3f866952e0223f164e23338a3ab3621875db5
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-17 17:30:51 +0000

    new

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,3 +6,4 @@ const url = 'https://gpabooster-api.herokuapp.com/exams'
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
 export const deletePost = (id) => axios.delete(`${url}/${id}`)
+export const updatePost = (id, post) => axios.patch(`${url}/${id}`, post)
