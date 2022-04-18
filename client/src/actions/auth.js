7e68562c155fbbd4060f99c22c184212b6d4f474
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-18 20:31:15 +0000

    nw

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,21 +1,22 @@
 import * as api from '../api'
+// import { signin, signup } from './auth'
 
 export const signin = (formData, history) => async (dispatch) => {
-  //   try {
-  //     const { data } = await api.signin(formData)
-  //     dispatch({ type: 'AUTH', data })
-  //     history.push('/')
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
+  try {
+    const { data } = await api.signIn(formData)
+    dispatch({ type: 'AUTH', data })
+    history.push('/')
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export const signup = (formData, history) => async (dispatch) => {
-  //   try {
-  //     const { data } = await api.signin(formData)
-  //     dispatch({ type: 'AUTH', data })
-  //     history.push('/')
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
+  try {
+    const { data } = await api.signUp(formData)
+    dispatch({ type: 'AUTH', data })
+    history.push('/')
+  } catch (error) {
+    console.error(error)
+  }
 }
