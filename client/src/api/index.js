1bce1e7034a9211d9edf6ffc9ac94c2b81a8c8a0
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-18 20:27:11 +0000

    new

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 
 const API = axios.create({ baseURL: 'https://gpabooster-api.herokuapp.com' })
 
-// const url = 'https://gpabooster-api.herokuapp.com/exams'
-// const url = 'http://localhost:5000/exams'
-
 export const fetchPosts = () => API.get('/exams')
 export const createPost = (newPost) => API.post('/exams', newPost)
 export const deletePost = (id) => API.delete(`/exams/${id}`)
 export const updatePost = (id, post) => API.patch(`/exams/${id}`, post)
+
+export const signIn = (formData) => API.post('/user/signin', formData)
+export const signUp = (formData) => API.post('/user/signup', formData)
