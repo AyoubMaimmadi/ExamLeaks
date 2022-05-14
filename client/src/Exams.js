592238c2aa62047a23beff653348e1d8c88b7b5b
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-14 15:06:08 +0100

    new

--- client/src/Exams.js
+++ client/src/Exams.js
@@ -9,13 +9,14 @@ import Navbar from './Components/Navbar'
 // Used to fetch the data from the global redux store
 import { useSelector } from 'react-redux'
 
+// we set an array of categories that has all the posts categories and an ALL category
+const allCategories = ['all', ...new Set(posts.map((item) => item.category))]
+
 export const Exams = () => {
   // we initialize useSelector as a hook, where we get access the to whole redux store
   // and we extract the posts from the store that we exported from the reducers/index.js
   const posts = useSelector((state) => state.posts)
 
-  // we set an array of categories that has all the posts categories and an ALL category
-  const allCategories = ['all', ...new Set(posts.map((item) => item.category))]
   // we set useState Hooks to be able to access them from the component
   const [examItems, setExamItems] = useState([posts])
   const [categories, setCategories] = useState(allCategories)
@@ -29,7 +30,6 @@ export const Exams = () => {
 
   // When there is an input in the searchbar we change the name of the page
   // to Results and search for the target using simple array manipulation
-
   useEffect(() => {
     // the page name is set to Results when there is an input in the searchbar
     if (pagename === 'Results') {
@@ -49,6 +49,8 @@ export const Exams = () => {
         }
       })
     } else {
+      // when we delete the input from search bar when and page != Results
+      // we display all the posts back agagin because this time the input is empty
       setExamItems([])
       posts.filter((val) => {
         if (val.title.toLowerCase().includes(input.toLowerCase())) {
@@ -58,14 +60,16 @@ export const Exams = () => {
     }
   }, [input])
 
-  // Filter Items based on the School i.e. CSC, BA, ...
-
+  // Filter Cards based on the School i.e. CSC, BA, ...
   const filterItems = (category) => {
     if (category === 'all') {
+      // when the category is all, we display all the posts and return
       setExamItems(posts)
       return
     }
+    // when the category is not all, we filter the posts based on the category
     const newItems = posts.filter((item) => item.category === category)
+    // we set the exam items to be the filtered items
     setExamItems(newItems)
   }
 
--- client/src/Homework.js
+++ client/src/Homework.js
@@ -1,32 +1,45 @@
 import React, { useState, useEffect } from 'react'
+
+// componet imports
 import Exam from './Components/Exam'
 import Categories from './Components/Categories'
-import items from './data/HomeWorkData'
 import Uparrow from './Components/Uparrow'
 import Navbar from './Components/Navbar'
+// for Homework, we have local data, not from database
+import items from './data/HomeWorkData'
 
+// we set an array of categories that has all the posts categories and an ALL category
 const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 
 const Homewok = () => {
+  // we set useState Hooks to be able to access them from the component
   const [examItems, setExamItems] = useState(items)
   const [categories, setCategories] = useState(allCategories)
   const [pagename, setPagename] = useState('Homework')
   const [input, setInput] = useState('')
 
+  // When there is an input in the searchbar we change the name of the page
+  // to Results and search for the target using simple array manipulation
   useEffect(() => {
+    // the page name is set to Results when there is an input in the searchbar
     if (pagename === 'Results') {
+      // we 1st set the exam homwwork that will be displayed to an empty array
       setExamItems([])
       items.filter((val) => {
+        // we check if the input is in the title or name or category or id of the homwwork item
         if (
           val.title.toLowerCase().includes(input.toLowerCase()) ||
           val.name.toLowerCase().includes(input.toLowerCase()) ||
           val.category.toLowerCase().includes(input.toLowerCase()) ||
           val.id == parseInt(input)
         ) {
+          // if true,  we add it to the examItems array to be displayed
           setExamItems((examItems) => [...examItems, val])
         }
       })
     } else {
+      // when we delete the input from search bar when and page != Results
+      // we display all the posts back agagin because this time the input is empty
       setExamItems([])
       items.filter((val) => {
         if (val.title.toLowerCase().includes(input.toLowerCase())) {
@@ -37,13 +50,15 @@ const Homewok = () => {
   }, [input])
 
   // Filter Items based on their school: CSC, BA, ...
-
   const filterItems = (category) => {
     if (category === 'all') {
+      // when the category is all, we display all the posts and return
       setExamItems(items)
       return
     }
+    // when the category is not all, we filter the posts based on the category
     const newItems = items.filter((item) => item.category === category)
+    // we set the exam items to be the filtered items
     setExamItems(newItems)
   }
 
