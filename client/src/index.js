690421d67fae624b322627078382bca2f4996f52
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-14 15:08:52 +0100

    new

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+// CSC imports
 import './css/index.css'
 import './css/searchBar.css'
 import './css/reviews.css'
+
+// Main APP import
 import App from './App'
+
+// Redux imports
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
+// react router dom imports
 import { BrowserRouter as Router } from 'react-router-dom'
 
+// import reducers
 import reducers from './reducers'
+// create the application store, which will hold the state of the application data
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
+// We render the application to the DOM
+// We wrap the application in a Provider component, which allows us
+// to access the store from any component, and we pass the store as a prop
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
